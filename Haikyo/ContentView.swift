742b4d899820f07e9e8f372357a7c9b8d30ff7c9
iamgabrielma <gabrielmaldonado.art@gmail.com>
2021-08-15 14:46:05 +0700

    Initial sharing implementation

--- Haikyo/ContentView.swift
+++ Haikyo/ContentView.swift
@@ -11,9 +11,23 @@ import WidgetKit
 struct ContentView: View {
     
     @ObservedObject private var dailyHaiku = Haiku()
+    @State private var isSharing = false
+    
+    func shareButton() {
+        isSharing.toggle()
+        
+        let testURL = URL(string: "https://apple.com")
+        let sharingActivity = UIActivityViewController(activityItems: [testURL!], applicationActivities: nil)
+        
+        UIApplication.shared.windows.first?.rootViewController?.present(sharingActivity, animated: true, completion: nil)
+        
+    }
     
     var body: some View {
         HaikuView()
+        Button(action: shareButton){
+            Image(systemName: "square.and.arrow.up")
+        }
     }
 }
 
