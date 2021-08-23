9da95c4ed19f9af8264dfdf6bad966a2fcfb8576
iamgabrielma <gabrielmaldonado.art@gmail.com>
2021-08-23 11:06:47 +0200

    Added new color constants & update readme

--- Haikyo/Constants.swift
+++ Haikyo/Constants.swift
@@ -11,14 +11,14 @@ import UIKit
 enum HaikuColors {
     
     // Array of all Combinations:
-    static let haikuStyles: [[UIColor]] = [haikuBgCream, haikuBgGrey, haikuBgPurple]
+    static let haikuStyles: [[UIColor]] = [haikuBgCream, haikuBgGrey, haikuBgPurple, haikuBgLime, haikuBgGreen, haikuBgPink]
     
     // Background Colors:
-    static var haikuBgCream: [UIColor] = [
+    static let haikuBgCream: [UIColor] = [
         UIColor(red: 0.98, green: 0.82, blue: 0.76, alpha: 1.00),
         UIColor(red: 0.28, green: 0.80, blue: 0.95, alpha: 1.00)
     ]
-    static var haikuBgGrey: [UIColor] = [
+    static let haikuBgGrey: [UIColor] = [
         UIColor.systemGray2,
         UIColor.systemGray6
     ]
@@ -26,10 +26,19 @@ enum HaikuColors {
         UIColor(red: 0.83, green: 0.77, blue: 0.98, alpha: 1.00),
         UIColor(red: 0.98, green: 0.82, blue: 0.76, alpha: 1.00)
     ]
-    static let haikuBgPink: [UIColor] = []
+    static let haikuBgLime: [UIColor] = [
+        UIColor(red: 217/255, green: 237/255, blue: 146/255, alpha: 1.00),
+        UIColor(red: 153/255, green: 217/255, blue: 140/255, alpha: 1.00)
+    ]
+    static let haikuBgGreen: [UIColor] = [
+        UIColor(red: 118/255, green: 200/255, blue: 147/255, alpha: 1.00),
+        UIColor(red: 52/255, green: 160/255, blue: 164/255, alpha: 1.00)
+    ]
+    static let haikuBgPink: [UIColor] = [
+        UIColor(red: 255/255, green: 198/255, blue: 255/255, alpha: 1.00),
+        UIColor(red: 189/255, green: 178/255, blue: 255/255, alpha: 1.00),
+    ]
     
     // Font Colors:
-    static var haikuFont: [UIColor: UIColor] = [:]
-    
-    
+    static let haikuFont: [UIColor: UIColor] = [:]
 }
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-#Haikyo
+# Haikyo
 
 Haikyo is a minimalistic iOS app built with SwiftUI that will show a Haiku every time you tap the screen, as well as show the Haiku of the Day via iOS widgets.
 
-![](https://tilcode.blog/wp-content/uploads/2021/08/2021-08-15-mvp.gif)
+| App Screen View |
+|---------------------|
+|![](https://tilcode.blog/wp-content/uploads/2021/08/Simulator-Screen-Recording-iPhone-8-2021-08-23-at-10.58.25.gif)|
+
+| Widgets Screen View |
+|---------------------|
+|![](https://tilcode.blog/wp-content/uploads/2021/08/2021-08-15-mvp.gif)|
 
 Used:
 
